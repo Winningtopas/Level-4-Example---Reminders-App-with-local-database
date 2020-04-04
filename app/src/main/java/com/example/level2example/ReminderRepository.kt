57e711d090b6fc2aa3cf25c24c24ca24bc169c7c
Winningtopas <jay07@live.nl>
2020-04-04 14:13:24 +0200

    Step 1

---
+++ app/src/main/java/com/example/level2example/ReminderRepository.kt
@@ -0,0 +1,32 @@
+package com.example.level2example
+
+import android.content.Context
+import com.example.level2example.ReminderRoomDatabase.ReminderRoomDatabase //haal dit weg als het stuk is
+
+
+public class ReminderRepository(context: Context) {
+
+    private var reminderDao: ReminderDao
+
+    init {
+        val reminderRoomDatabase = ReminderRoomDatabase.getDatabase(context)
+        reminderDao = reminderRoomDatabase!!.reminderDao()
+    }
+
+    fun getAllReminders(): List<Reminder> {
+        return reminderDao.getAllReminders()
+    }
+
+    fun insertReminder(reminder: Reminder) {
+        reminderDao.insertReminder(reminder)
+    }
+
+    fun deleteReminder(reminder: Reminder) {
+        reminderDao.deleteReminder(reminder)
+    }
+
+    fun updateReminder(reminder: Reminder) {
+        reminderDao.updateReminder(reminder)
+    }
+
+}
